58dab79b36e36a3c29c757e7bb11749a9714052e
valentinaiori <valentinaiori@live.it>
2022-08-25 19:17:24 +0200

    Implementation

--- script.js
+++ script.js
@@ -10,7 +10,20 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 // stampa numeri da 1 a 100
 
 for(i=1; i<=100; i++){
-    console.log(i)
+    
+
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log('FizzBuzz', i)
+    } else if (i % 3 === 0){
+        console.log('Fizz', i)
+    } else if (i % 5 === 0 ){
+        console.log('Buzz', i)
+    } else {
+        console.log(i)
+    }
+        
 
 }
+ 
 
+//
